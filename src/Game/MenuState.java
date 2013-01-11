baff7f1656feffbc60117f73b0dcccf3c0a721c2
Masa <matti.sippola@aalto.fi>
2013-01-12 01:30:20 +0200

    Bäängg!! Pistelasku kunnossa
    1.Scrollaus nopeuden muuttuminen on helposti säädeltävissä.
    2. Pistelasku GameEnginen hommaksi
    3. Avatarin nopeus koostuu perus osasta ja JumpingBonuksesta
    4. MenuState näyttää viimeisimmät tulokset, GamePlayState kertoo
    viimeisimmän tuloksen Menulle ennen kuin GameEngine nollataan

--- src/Game/MenuState.java
+++ src/Game/MenuState.java
@@ -1,6 +1,8 @@
 package game;
 
 import java.awt.Font;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.newdawn.slick.Color;
 import org.newdawn.slick.GameContainer;
@@ -10,6 +12,7 @@ import org.newdawn.slick.Input;
 import org.newdawn.slick.SlickException;
 import org.newdawn.slick.UnicodeFont;
 import org.newdawn.slick.font.effects.ColorEffect;
+import org.newdawn.slick.geom.Point;
 import org.newdawn.slick.state.BasicGameState;
 import org.newdawn.slick.state.StateBasedGame;
 import org.newdawn.slick.state.transition.FadeInTransition;
@@ -20,14 +23,19 @@ public class MenuState extends BasicGameState {
 
 	private int stateID;
 	
-	String menuText;
-	UnicodeFont font;
-	Image background;
+	private List<Integer>lastScores;
+	//kuinka monta tulosta naytetaan enimmillaan yhta aikaa
+	private final int scoresMax = 5;
+	
+	private String menuText;
+	private UnicodeFont font;
+	private Image background;
 	
 	
 	public MenuState(int stateID) {
 		this.stateID = stateID;
 		this.menuText = "Paina mua, jos mielit pelata";
+		this.lastScores = new ArrayList<Integer>();
 	}
 
 	@Override
@@ -51,6 +59,7 @@ public class MenuState extends BasicGameState {
 	@Override
 	public void render(GameContainer container, StateBasedGame game, Graphics g)
 			throws SlickException {
+		this.showLastScores(g, new Point(800,40));
 		this.font.drawString(200, Game.HEIGHT - 200, this.menuText);
 		g.setBackground(Color.white);
 		g.drawImage(this.background, 10, 10);
@@ -83,6 +92,41 @@ public class MenuState extends BasicGameState {
 		Transition leave = new FadeOutTransition();
 		Transition enter = new FadeInTransition();
 		game.enterState(state, leave, enter);
+	}
+	
+	public void addScore(int score){
+		this.lastScores.add(score);
+	}
+	
+	public void showLastScores(Graphics g, Point point){
+		//tulos rivien vŠlinen etŠisyys
+		float distBetweenLines = 50f;
+		
+		float X = point.getX();
+		float Y = point.getY();
+		
+		//kuinka monta on jo piirretty ruudulle
+		float scoresShown = 0;
+		int scores = this.lastScores.size(); 
+		if (scores == 0){
+			return;
+		}
+		
+		font.drawString(X, Y, "NŠin korkealle olet kivunnut");
+		
+		for (int i = scores-1; i >= 0 && scoresShown < this.scoresMax; i--){
+		
+			
+			float x = X + 20;
+			float y = Y + (scoresShown+1.0f) * distBetweenLines;
+			String scoreTxt = Integer.toString(this.lastScores.get(i));
+			font.drawString(x, y, scoreTxt);
+			scoresShown++;
+		}
+	}
+	
+	public void updateMenuText(){
+		this.menuText = "Hihii, uudestaan!";
 	}
 
 }
